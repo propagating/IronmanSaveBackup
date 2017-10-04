9647e13ec8fc334453ba1965e1d7eca7def0e56d
Ryan Richardson <rrichardson@setecsoftware.com>
2017-10-03 22:32:18 -0700

    First commit with UI version 1.0

--- IronmanSaveBackup/MainWindow.xaml.cs
+++ IronmanSaveBackup/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,9 +22,25 @@ namespace IronmanSaveBackup
     /// </summary>
     public partial class MainWindow : Window
     {
+        public const string xcom2RegexFind= @"(^save_IRONMAN_Campaign_.*$)";
+        public const string xcom2RegexCampaign = @"^save_IRONMAN_Campaign_(.*)$"; //Match[0] for save
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BackupTextbox_Click(object sender, MouseButtonEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            backupTextbox.Text = dialog.SelectedPath;
+        }
+
+        private void SaveTextbox_Click(object sender, MouseButtonEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            saveTextbox.Text = dialog.SelectedPath;
         }
     }
 }
